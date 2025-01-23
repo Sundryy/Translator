5b6a524d53fa481a451de6cd0acd7aac95803ff9
Jordan Farrow <jordomine01@outlook.com>
2025-01-23 20:03:03 +0800

    Red box now correctly embodies the users selected area.

--- Translator.py
+++ Translator.py
@@ -9,42 +9,34 @@ lastX = 0
 lastY = 0
 choosingRectangle = False
 
-topLeft = (firstX,firstY)
-bottomRight = (lastX,lastY)
+
+
 colorLine = (0,0,255)
 lineThickness = 10
 
 def capture_event(event,x,y,flags,params):
-    global firstX,firstY,lastX,lastY, choosingRectangle, topLeft, bottomRight, colorLine, lineThickness
+    global firstX,firstY,lastX,lastY, choosingRectangle, colorLine, lineThickness
+    #Finds top-left coordinate of chosen area
     if event == cv2.EVENT_LBUTTONDOWN:
         choosingRectangle = True
-        firstX = x
-        firstY = y
-        print(x)
+        if firstX == 0:
+            firstX = x
+            firstY = y
     
     elif event == cv2.EVENT_MOUSEMOVE and choosingRectangle:
         lastX = x
         lastY = y
-        print(lastX)
-        #for some reason i cant get the red rectangle to appear...
+        #Shows user where they are selecting part of the image
         imageCopy = image.copy()
-        cv2.rectangle(imageCopy,
-                      topLeft,
-                      bottomRight,
-                      colorLine,
-                      lineThickness)
-        cv2.imshow('image2', imageCopy)
+        cv2.rectangle(imageCopy, (firstX,firstY), (lastX,lastY), colorLine, lineThickness)
+        cv2.imshow('image', imageCopy)
 
-
-    #takes new screenshot of contents within coordinates
+    #takes screenshot of selected area
     elif event == cv2.EVENT_LBUTTONUP:
         choosingRectangle = False
+        chosenImage = image[firstY:lastY, firstX:lastX]
+        cv2.imshow('Chosen Image',chosenImage)
         
-        print('dog')
-        newImage = image[firstY:lastY, firstX:lastX]
-        cv2.destroyAllWindows
-        cv2.imshow('new',newImage)
-    
 image = pyautogui.screenshot()
 image = numpy.array(image)
 image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
