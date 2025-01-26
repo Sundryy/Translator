897b968ea8b913aa03be07fee67b165fddab2ac1
Jordan Farrow <jordomine01@outlook.com>
2025-01-26 15:22:32 +0800

    made progress on GUI and fixed a few issues

--- Translator.py
+++ Translator.py
@@ -3,21 +3,21 @@ import pyautogui
 import numpy
 import time
 #fix up this gross mess
-import tkinter as tk
+from tkinter import *
 from tkinter import messagebox
 from PIL import Image, ImageTk
 #the other stuff is fine.
 from pytesseract import pytesseract
-from googletrans import Translator
+from googletrans import Translator, LANGUAGES
 import asyncio
 
 pytesseract.tesseract_cmd = "/opt/homebrew/bin/tesseract"
 
 #beginning coordinates
-firstX = -1
-firstY = -1
-lastX = -1
-lastY = -1
+firstX = 0
+firstY = 0
+lastX = 0
+lastY = 0
 choosingRectangle = False
 translator = Translator()
 
@@ -51,23 +51,34 @@ def capture_event(event,x,y,flags,params):
             #Occurs if x and y cords of both points are different.
             untranslatedWords = pytesseract.image_to_string(chosenImage)
             translatedWords, predictedLanguage = asyncio.run(translate(untranslatedWords))
-            print(translatedWords, predictedLanguage) #remove this eventually. for testing purposes
+            #If no words are detected, it will return to last button press GUI state.
+            if translatedWords == '': 
+                raise Exception('No text detected in the selected area.')
 
-            #Removes old GUI elements and creates new ones to display translation and selected area.
-            instructionsBtn.pack_forget(), startBtn.pack_forget(), frontName.pack_forget()
-            visibleTranslation = tk.Label(gui, text= 'this is ' + translatedWords).pack()
-            
             chosenImage = cv2.cvtColor(chosenImage, cv2.COLOR_BGR2RGB)
-            im = ImageTk.PhotoImage(image=Image.fromarray(chosenImage))
-            translatedImageTk = tk.Label(gui, image=im)
-            translatedImageTk.pack()
+            im = Image.fromarray(chosenImage)
+            #Ensures image fits within the GUI window
+            if im.size[0] > 750:
+                
+                im = im.resize((750,im.size[1])) 
+            #Ensures image fits within the GUI window
+            if im.size[1] > 450:
+                im = im.resize((im.size[0],450)) 
+
+            im = ImageTk.PhotoImage(image=im)
             translatedImageTk.image = im
+            #Changing GUI widgets
+            translatedImageTk.config(image=im)
+            visibleTranslation.config(text=translatedWords)
+            translationTitle.config(text='Translation from ' + LANGUAGES[predictedLanguage])
+            instructionsBtn.place_forget(), screenShotBtn.place_forget(), frontName.place_forget()
+            translatedImageTk.pack(pady=20), translationTitle.pack(), visibleTranslation.pack(), screenShotBtn.pack(side=LEFT,padx=125), reselectAreaBtn.pack(side=LEFT)
             gui.deiconify(), gui.update()
 
         except:
             #Occurs if no area is chosen/ x and y cords of both points are the same,
             #causing program to go back to GUI at the beginning.
-            messagebox.showerror('Error', 'An area has not been selected. Please try again.')
+            messagebox.showerror('Error', 'Words have not been detected. Please try again.')
             gui.deiconify()
             gui.update()
 
@@ -99,10 +110,14 @@ def center_window(window):
 
 #Instructions for how to use the program.
 def loadInstructions():
-    startBtn.pack_forget(), instructionsBtn.pack_forget(), frontName.pack_forget()
-    thankYou = tk.Label(gui, text='Welcome and thank you using my program :)')
-    instructions = tk.Label(gui, text='1. Screenshot your main monitors screen by pressing the "Screenshot" button.\n2. Click and drag your mouse from the top-left to the bottom-right of what you wish to translate.\n3. Recieve your translation!')
-    thankYou.pack(), instructions.pack()
+    screenShotBtn.place_forget(), instructionsBtn.place_forget(), frontName.place_forget()
+    thankYou.pack(), instructions.pack(pady=10), backBtn.pack()
+
+def back():
+    thankYou.pack_forget(), instructions.pack_forget(), backBtn.pack_forget()
+    frontName.place(relx=0.5, rely=0.5, anchor=CENTER)
+    screenShotBtn.place(relx=0.65, rely=0.6, anchor=CENTER) 
+    instructionsBtn.place(relx=0.35, rely=0.6, anchor=CENTER)
 
 #see if this works
 def retakeImage():
@@ -110,22 +125,46 @@ def retakeImage():
     cv2.setMouseCallback('image', capture_event)
 
 
-gui = tk.Tk()
+gui = Tk()
 gui.title('Screenshot-to-Text Translator')
+gui.geometry('850x600')
 center_window(gui)
 
-startBtn = tk.Button(gui, text="Screenshot", command=imageCreation)
-instructionsBtn = tk.Button(gui, text="Instructions", command=loadInstructions)
-startBtn.pack(pady=10)
-instructionsBtn.pack(pady=10)
+screenShotBtn = Button(gui, text="Screenshot", command=imageCreation, font=('Helvetica', 35))
+instructionsBtn = Button(gui, text="Instructions", command=loadInstructions, font=('Helvetica', 35))
 
-frontName = tk.Label(gui, text= 'this will be the name of the program.')
-frontName.pack(pady=10)
-ChangeRectangleBtn = tk.Button(gui, text='change area', command=retakeImage)
+translationTitle = Label(gui, text='Translation from', font=('',35))
+thankYou = Label(gui, text='Welcome and thank you using my program :)')
+instructions = Label(gui, text='1. Screenshot your main monitors screen by pressing the "Screenshot" button.\n2. Click and drag your mouse from the top-left to the bottom-right of what you wish to translate.\n3. Recieve your translation!')
+translatedImageTk = Label(gui)
+visibleTranslation = Label(gui)
+frontName = Label(gui, text= 'Screenshot-to-Text Translator',font=('helvetica',50,'bold'))
+reselectAreaBtn = Button(gui,text='Reselect Area', command=retakeImage, font=('Helvetica', 35))
+backBtn = Button(gui,text='Back', command=back)
+'''
+frontName.place(relx=0.5, rely=0.5, anchor=CENTER)
+screenShotBtn.place(relx=0.65, rely=0.6, anchor=CENTER) 
+instructionsBtn.place(relx=0.35, rely=0.6, anchor=CENTER)
+'''
+
+back()
 gui.mainloop()
+
 
 
 #going to show the image taken, ask if its all good, if it isnt, then do process again.
 #if it is good 
 
 
+#do initial gui setup code
+
+#do labels code
+
+#then buttons code
+
+#then stuff being packed code.
+
+
+#use image.width()
+#use image.height()
+#these are for scaling the image to fit within the bounds that i want.
